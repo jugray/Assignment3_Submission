1ac86c26cfd412905d89702afa0f22c82160fcb8
Justin Gray <jugray@gmail.com>
2023-11-04 22:15:55 -0500

    Initial Commit

---
+++ Assignment3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment3
+//
+//  Created by Justin Gray on 11/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
